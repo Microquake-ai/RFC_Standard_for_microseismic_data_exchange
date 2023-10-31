8725ac942426b684feda9d3cab2b9dc555a69455
GitHub Action <action@github.com>
2023-10-31 09:05:42 +0000

    Add generated HTML page

---
+++ docs/labels.pl
@@ -0,0 +1,37 @@
+# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# Associate labels original text with physical files.
+
+
+$key = q/app:coordinate_system_handling/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:asdf_data_format/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:app_response/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:app_source_params/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:event_type_mapping/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:source_parameters/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# labels from external_latex_labels array.
+
+
+1;
+
